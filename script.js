b49f2b7344a251ab51191a0e108970dc67cd36ec
Anurag Bachheti <157354714+Anurag-Bachheti@users.noreply.github.com>
2025-09-19 13:56:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-function fibonacci(num) {
-// your code here
+function fibonacci(n) {
+  // handle small cases
+  if (n === 0) return [];
+  if (n === 1) return [0];
+
+  let series = [0, 1];
+
+  for (let i = 2; i < n; i++) {
+    series[i] = series[i - 1] + series[i - 2];
+  }
+
+  return series.slice(0, n);
 }
+
+console.log(fibonacci(2)); // [0, 1]
 
 module.exports = fibonacci;
