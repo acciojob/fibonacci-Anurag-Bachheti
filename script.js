855f22791126d7d451395cde101c161be7cb6e19
Anurag Bachheti <157354714+Anurag-Bachheti@users.noreply.github.com>
2025-09-19 19:01:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function fibonacci(n) {
-  if(n<=1){
-	  return 0;
-  }
 
+ if (n === 1) return 0;
+  if (n === 2) return 1;
+	
 	let a = 0, b = 1;
 
 	if(n === 1)return a;
 	if(n === 2)return b;
 
-	for(let i=3; i<n; i++){
+	for(let i=3; i<=n; i++){
 		[a,b] = [b, a+b]
 	}
-	return b
+	return b;
 }
 
 console.log(fibonacci(2));
